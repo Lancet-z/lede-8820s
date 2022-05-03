c0d3c76715b02d68f941e3c22fb6ec72ac329e9d
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-05-03 22:21:29 +0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -6,7 +6,7 @@ rm -rf ./package/lean/luci-theme-argon
 #sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
 #sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 # 科学插件
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
